0a4fd44231730cb726c3e8271ed2283d13baf913
Nikolaj Reibke <nikolajsr12@gmail.com>
2015-09-23 13:50:58 +0200

    Adding code
    Code is awesome

---
+++ Stack/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack<T>
+    {
+        private List<T> list;
+
+        public Stack()
+        {
+            list = new List<T>();
+        }
+
+        public T Pop()
+        {
+            T t = list.Last();
+
+            list.RemoveAt(list.Count - 1);
+
+            return t;
+        }
+
+        public void Push(T t)
+        {
+            list.Add(t);
+        }
+
+        public T Peek()
+        {
+            return list.Last();
+        }
+
+        public int Size()
+        {
+            return list.Count();
+        }
+
+        public bool IsEmpty()
+        {
+            return list.Count == 0;
+        }
+    }
+}
---
+++ StackTest/StackTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stack;
+using System;
+
+namespace Stack
+{
+    [TestClass]
+    public class StackTest
+    {
+        Stack<int> stack;
+
+        [TestInitialize]
+        public void init()
+        {
+            stack = new Stack<int>();
+        }
+        
+        [TestMethod]
+        public void TestIsEmpty()
+        {
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void TestPush()
+        {
+            stack.Push(1);
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void TestPop()
+        {
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Pop());
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void TestPeek()
+        {
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Peek());
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        [TestMethod, ExpectedException(typeof(InvalidOperationException))]
+        public void TestEmptyPeek()
+        {
+            stack.Peek();
+        }
+
+        [TestMethod, ExpectedException(typeof(InvalidOperationException))]
+        public void TestEmptyPop()
+        {
+            stack.Pop();
+        }
+
+        [TestMethod]
+        public void TestSize()
+        {
+            Assert.AreEqual(0, stack.Size());
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Size());
+            stack.Push(1);
+            Assert.AreEqual(2, stack.Size());
+            stack.Pop();
+            Assert.AreEqual(1, stack.Size());
+            stack.Pop();
+            Assert.AreEqual(0, stack.Size());
+        }
+
+        [TestMethod]
+        public void TestReserve()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                stack.Push(i);
+            }
+            for (int i = 100; --i >= 0;)
+            {
+                Assert.AreEqual(i, stack.Pop());
+            }
+        }
+    }
+}
